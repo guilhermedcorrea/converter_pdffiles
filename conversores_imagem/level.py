db96e2dfb5fed6f461c67a283298625f5e76ce8c
guilhermedcorrea <78979736+guilhermedcorrea@users.noreply.github.com>
2022-03-18 08:36:33 -0300

    Update level.py

--- conversores_imagem/level.py
+++ conversores_imagem/level.py
@@ -13,10 +13,10 @@ filename = level + " - " + \
            time.strftime("%d-%b").replace(":", ".") + ".csv"
 
 conn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
-                      "Server=w2019.hausz.com.br;"
-                      "Database=HauszMapa;"
-                      "UID=Aplicacao;"
-                      "PWD=S3nh4Apl!caca0")
+                      "Server=;"
+                      "Database=;"
+                      "UID=;"
+                      "PWD=")
 
 select_bd = """
         SELECT 
